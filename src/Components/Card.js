dbf2765cc361831d9521ab84cd1289a24ba6b4cb
kor-Don <dy950328@gmail.com>
2019-08-24 15:06:58 +0900

    little more css

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,6 +7,7 @@ const Container = styled.div`
   height: 300px;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
   border-radius: 4px;
+  position: relative;
 
   /* PULSE */
   @keyframes pulse {
@@ -37,8 +38,32 @@ const Image = styled.div`
   transition: opacity 0.1s linear;
 `;
 
+const Get = styled.div`
+  width: 300px;
+  height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  opacity: 0;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+const Text = styled.span`
+  color: white;
+  font-family: "Anton", sans-serif;
+  font-size: 40px;
+  text-transform: uppercase;
+`;
+
 const Card = ({ imageUrl, id }) => (
   <Container id={id}>
+    <Get>
+      <Text>add</Text>
+    </Get>
     <Image
       bgUrl={imageUrl ? imageUrl : null}
       onClick={event => console.log(imageUrl)}
