e536f5eb404f1c88f30b4cf948eb129d616f50ea
kor-Don <dy950328@gmail.com>
2019-08-24 14:53:24 +0900

    Card Edit + getUrl

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,6 +5,26 @@ import PropTypes from "prop-types";
 const Container = styled.div`
   width: 100%;
   height: 300px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  border-radius: 4px;
+
+  /* PULSE */
+  @keyframes pulse {
+    50% {
+      transform: scale(0.9);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  &:hover {
+    animation-name: pulse;
+    animation-duration: 1s;
+    animation-timing-function: ease-out;
+    animation-iteration-count: infinite;
+    animation-direction: reverse;
+  }
 `;
 
 const Image = styled.div`
@@ -19,7 +39,10 @@ const Image = styled.div`
 
 const Card = ({ imageUrl, id }) => (
   <Container id={id}>
-    <Image bgUrl={imageUrl ? imageUrl : null} />
+    <Image
+      bgUrl={imageUrl ? imageUrl : null}
+      onClick={event => console.log(imageUrl)}
+    />
   </Container>
 );
 
